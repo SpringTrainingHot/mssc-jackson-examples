74c7666ed0a0e4f69b32842cb55932cb67f9f796
Антон Тараненко <taranenkoant@gmail.com>
2020-11-30 16:30:19 +0300

    initial commit

---
+++ src/main/java/ru/taranenkoant/mssc_jackson_examples/MsscJacksonExamplesApplication.java
@@ -0,0 +1,13 @@
+package ru.taranenkoant.mssc_jackson_examples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscJacksonExamplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsscJacksonExamplesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/taranenkoant/mssc_jackson_examples/model/BeerDtoTest.java
@@ -0,0 +1,7 @@
+package ru.taranenkoant.mssc_jackson_examples.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BeerDtoTest {
+
+}
